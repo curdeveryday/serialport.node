be411dc74a93db29b6d553abcadfbff08a7e4e9f
Hugh Young <young9426@163.com>
2021-05-07 16:50:25 +0800

    Add github actions

---
+++ scripts/upload-bin.js
@@ -0,0 +1,37 @@
+const OSS = require('ali-oss');
+const path = require('path');
+const os = require('os');
+const cliArgs = require('yargs').argv;
+
+const ELECTRON_VERSION = '12.0.4';
+
+const config = {
+    region: cliArgs.REGION,
+    accessKeyId: cliArgs.ACCESS_KEY_ID,
+    accessKeySecret: cliArgs.ACCESS_KEY_SECRET,
+    bucket: cliArgs.BUCKET,
+    internal: false
+}
+
+const bin = cliArgs.TYPE === 'serialport'
+            ? path.join(__dirname, '..', 'node_modules', '@serialport', 'bindings', 'build', 'Release', 'bindings.node')
+            : path.join(__dirname, '..', 'node_modules', 'usb-detection', 'build', 'Release', 'bindings.node')
+
+let client = new OSS({
+    region: config.region,
+    accessKeyId: config.accessKeyId,
+    accessKeySecret: config.accessKeySecret,
+    bucket: config.bucket,
+    internal: config.internal
+});
+
+function getName(binType) {
+    const prefix = os.platform() === 'darwin' ? 'darwin' : os.platform() === 'win32' ? 'win32' : 'Ubuntu18.04';
+    return `${binType}_${prefix}_${ELECTRON_VERSION}_x64.node`;
+}
+
+
+client.put(getName(cliArgs.TYPE), bin)
+.catch(err => {
+    console.log(err);
+});
